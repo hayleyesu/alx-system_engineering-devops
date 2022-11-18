3afe594053fc07f23a3e8f34b290f7c9a0822fe9
hayleyesu <haylajul03@gmail.com>
2022-11-18 15:56:52 +0300

    tasks added

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Python script that returns information using a REST API
+"""
+
+import requests
+from sys import argv
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    userId = argv[1]
+    user_info = requests.get(f"{url}users/{userId}").json()
+    employee_name = user_info.get('name')
+    all_tasks = requests.get(f"{url}todos?userId={userId}").json()
+    completed_tasks = []
+    for task in all_tasks:
+        if task.get('completed'):
+            completed_tasks.append(task)
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(employee_name, len(completed_tasks),
+                  len(all_tasks)))
+
+    for task in completed_tasks:
+        print(f"\t {task.get('title')}")
